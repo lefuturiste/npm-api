4fa1be84809dafe7d9d41dcb057aa7ffe5d57ff2
Matthieu Bessat <spamfree@matthieubessat.fr>
2018-06-29 23:50:27 +0200

    Using httper

--- App/Controllers/PackageController.php
+++ App/Controllers/PackageController.php
@@ -3,74 +3,75 @@
 namespace App\Controllers;
 
 use GuzzleHttp\Client;
+use Httper\Request;
+use Httper\Uri;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Validator\Validator;
 
 class PackageController extends Controller
 {
-	public function getSearch(ServerRequestInterface $request, ResponseInterface $response)
-	{
-		$validator = new Validator($request->getQueryParams());
-		$validator->required('query');
-		$validator->notEmpty('query');
-		if ($validator->isValid()) {
-			$client = new Client();
-			$queryParams = "?q={$validator->getValue('query')}";
-			if ($validator->getValue('page') !== NULL && $validator->getValue('perPage') !== NULL && !empty($validator->getValue('page')) && !empty($validator->getValue('perPage'))) {
-				$queryParams .= "&page={$validator->getValue('page')}&perPage={$validator->getValue('perPage')}";
-			}
-			if ($validator->getValue('ranking') !== NULL && !empty($validator->getValue('ranking'))) {
-				$queryParams .= "&ranking={$validator->getValue('ranking')}";
-			}
-			try {
-				$searchResponse = $client->get($this->container->get('npm')['endpoint'] . "/search" . $queryParams, [
-					'headers' => [
-						'x-spiferack' => true
-					]
-				]);
-			} catch (\Exception $e) {
-				return $response->withJson([
-					'success' => false
-				])->withStatus($e->getCode());
-			}
+    public function getSearch(ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $validator = new Validator($request->getQueryParams());
+        $validator->required('query');
+        $validator->notEmpty('query');
+        if ($validator->isValid()) {
+            $queryParams = "?q={$validator->getValue('query')}";
+            if ($validator->getValue('page') !== NULL && $validator->getValue('perPage') !== NULL && !empty($validator->getValue('page')) && !empty($validator->getValue('perPage'))) {
+                $queryParams .= "&page={$validator->getValue('page')}&perPage={$validator->getValue('perPage')}";
+            }
+            if ($validator->getValue('ranking') !== NULL && !empty($validator->getValue('ranking'))) {
+                $queryParams .= "&ranking={$validator->getValue('ranking')}";
+            }
+            $client = new \Httper\Client();
+            try {
+                $searchResponse = $client->request((new Request())
+                    ->withUri(new Uri($this->container->get('npm')['endpoint'] . "/search" . $queryParams))
+                    ->withHeader('x-spiferack', true)
+                );
+            } catch (\Exception $e) {
+                return $response->withJson([
+                    'success' => false
+                ])->withStatus($e->getCode());
+            }
 
-			$packages = json_decode($searchResponse->getBody(), 1)['objects'];
+            $packages = json_decode($searchResponse->getBody()->getContents(), 1)['objects'];
 
-			return $response->withJson([
-				'success' => true,
-				'data' => [
-					'count' => count($packages),
-					'packages' => $packages
-				]
-			]);
-		} else {
-			return $response->withJson([
-				'success' => false,
-				'errors' => $validator->getErrors()
-			]);
-		}
-	}
+            return $response->withJson([
+                'success' => true,
+                'data' => [
+                    'count' => count($packages),
+                    'packages' => $packages
+                ]
+            ]);
+        } else {
+            return $response->withJson([
+                'success' => false,
+                'errors' => $validator->getErrors()
+            ]);
+        }
+    }
 
-	public function getPackage($packageName, ServerRequestInterface $request, ResponseInterface $response)
-	{
-		$client = new Client();
-		try {
-			$packageResponse = $client->get($this->container->get('npm')['endpoint'] . "/package/{$packageName}", [
-				'headers' => [
-					'x-spiferack' => true
-				]
-			]);
-		} catch (\Exception $e) {
-			return $response->withJson([
-				'success' => false
-			])->withStatus($e->getCode());
-		}
-		$package = json_decode($packageResponse->getBody(), 1);
+    public function getPackage($packageName, ServerRequestInterface $request, ResponseInterface $response)
+    {
+        $client = new Client();
+        try {
+            $packageResponse = $client->get($this->container->get('npm')['endpoint'] . "/package/{$packageName}", [
+                'headers' => [
+                    'x-spiferack' => true
+                ]
+            ]);
+        } catch (\Exception $e) {
+            return $response->withJson([
+                'success' => false
+            ])->withStatus($e->getCode());
+        }
+        $package = json_decode($packageResponse->getBody(), 1);
 
-		return $response->withJson([
-			'success' => true,
-			'data' => $package
-		]);
-	}
+        return $response->withJson([
+            'success' => true,
+            'data' => $package
+        ]);
+    }
 }
--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ require '../vendor/autoload.php';
 | .env load
 |--------------------------------------------------------------------------
 */
-if (file_exists('.env')) {
+if (file_exists('../.env')) {
 	$dotenv = new Dotenv\Dotenv(dirname(__DIR__));
 	$dotenv->load();
 }
